f4880d52e70c8c1bb218f3a88f2307867bf74348
jozef-mitro <jozef.mitro.sk@gmail.com>
2016-02-06 11:18:23 +0100

    Change indentation to spaces only

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,15 @@
 
 int main(int argc, char *argv[])
 {
-	SDL_version compiled;
-	SDL_version linked;
+    SDL_version compiled;
+    SDL_version linked;
 
-	SDL_VERSION(&compiled);
-	SDL_GetVersion(&linked);
-	printf("We compiled against SDL version %d.%d.%d ...\n",
-	       compiled.major, compiled.minor, compiled.patch);
-	printf("But we are linking against SDL version %d.%d.%d.\n",
-	       linked.major, linked.minor, linked.patch);
+    SDL_VERSION(&compiled);
+    SDL_GetVersion(&linked);
+    printf("We compiled against SDL version %d.%d.%d ...\n",
+           compiled.major, compiled.minor, compiled.patch);
+    printf("But we are linking against SDL version %d.%d.%d.\n", 
+           linked.major, linked.minor, linked.patch);
 
-	return 0;
+    return 0;
 }
