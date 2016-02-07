53db149c4a658ea461e0d737fbec39719ded24ad
Kubo <viceriel@gmail.com>
2016-02-07 15:51:11 +0100

    Init commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,6 @@
 
 int main(int argc, char *argv[])
 {
-    SDL_version compiled;
-    SDL_version linked;
-
-    SDL_VERSION(&compiled);
-    SDL_GetVersion(&linked);
-    printf("We compiled against SDL version %d.%d.%d ...\n",
-           compiled.major, compiled.minor, compiled.patch);
-    printf("But we are linking against SDL version %d.%d.%d.\n", 
-           linked.major, linked.minor, linked.patch);
-
+    printf("Hello world!");
     return 0;
 }
